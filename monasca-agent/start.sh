276086f56cb413285b7632322f5fc7eb942184c6
Allan Wang <awang@op5.com>
2017-07-25 08:23:22 +0200

    saving

---
+++ monasca-agent/start.sh
@@ -0,0 +1,3 @@
+#!/bin/ash
+
+sudo monasca-setup --username monasca-agent --password password --project_name mini-mon --keystone_url http://10.5.0.4:5000/v3 --monasca_url http://10.5.0.12:8070/v2.0/
